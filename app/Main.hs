3cea161ffa8106b78193997c586d4619c3b8ca62
ignaciorodriguez10 <ignacio.rodriguez10@alu.uclm.es>
2024-05-09 18:17:03 +0200

    upgrades

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,24 @@
 module Main where
 
+import System.IO
 import Tokens
 import Grammar
 import Lib
-
 
 main :: IO ()
 main = do
     content <- readFile "app/example"
     let tokens = scanTokens content
+    putStrLn "Tokens:"
+    mapM_ (putStrLn . show) tokens
+    putStrLn "--------------------------------------------------------------------------------------------"
+
+    _ <- getLine -- Esperar a que el usuario introduzca cualquier letra y pulse Enter
+    putStrLn "\nARBOL SINTACTICO:"
     let ast = parse tokens
+    print ast
+    putStrLn "--------------------------------------------------------------------------------------------"
+    _ <- getLine -- Esperar a que el usuario introduzca cualquier letra y pulse Enter
+
+    putStrLn "INTERPRETACIÓN:"
     interpretar ast
